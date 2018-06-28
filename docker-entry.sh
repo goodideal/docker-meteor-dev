81d55d16301057cbeeff32b48e34de81928e58b0
Jerry <jerry@xqopen.cn>
2018-06-28 15:07:42 +0800

    add external mongodb, downgrage to 1.6.1.3

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
-/usr/local/mongodb/bin/mongod --fork --logpath /data --logappend
-export MONGO_URL=mongodb://localhost:27017/meteor
+if [ -z $EXT_MONGO ];then
+    echo "using internal mongodb"
+	/usr/local/mongodb/bin/mongod --fork --logpath /data --logappend
+    export MONGO_URL=mongodb://localhost:27017/meteor
+else
+	echo "using external mongodb"
+fi
 
 exec "$@"
